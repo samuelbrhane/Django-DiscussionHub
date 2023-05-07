eabf245a84c505f795282ed0120aeda40a8a61f2
samuelberhane <1samibrhane@gmail.com>
2023-05-07 15:27:12 +0300

    add search functionality based on title and description.

--- base/views.py
+++ base/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
+from django.db.models import Q
 
 
 # home page
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ""
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) | Q(
+        title__icontains=q) | Q(description__icontains=q))
     topics = Topic.objects.all()
     context = {"rooms": rooms, "topics": topics}
     return render(request, 'index.html', context)
