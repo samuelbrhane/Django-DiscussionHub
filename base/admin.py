a9325c8b7d02d9b4ba18b74a5654f0641be3c6ed
samuelberhane <1samibrhane@gmail.com>
2023-05-07 12:53:31 +0300

    Create Topic and Message model and make migrations.

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, Topic, Message
 
 admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
--- base/models.py
+++ base/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
+# general topics model
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+# rooms model
 class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=150)
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -9,3 +21,15 @@ class Room(models.Model):
 
     def __str__(self):
         return self.title
+
+
+# messages model
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    text = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} {self.text[0:40]}"
